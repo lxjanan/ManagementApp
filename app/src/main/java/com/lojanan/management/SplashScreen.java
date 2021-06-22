9b23994e7b9a3381edf48b9b8655c31c42e36cd8
lxjanan <17104@students.mrgs.school.nz>
2021-06-22 23:24:53 +1200

    SplashScreen.java Added -> Used VideoView

---
+++ app/src/main/java/com/lojanan/management/SplashScreen.java
@@ -0,0 +1,45 @@
+package com.lojanan.management;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.VideoView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    VideoView videoView;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(1);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        setContentView(R.layout.activity_splash_screen);
+
+        videoView = findViewById(R.id.viewVideo);
+
+        String path = "android.resource://com.lojanan.management/"+R.raw.splash_screen;
+
+        Uri uri = Uri.parse(path);
+        videoView.setVideoURI(uri);
+        videoView.setOnPreparedListener(MediaPlayer::start);
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            startActivity(new Intent(SplashScreen.this,Login.class));
+            finish();
+        },3000);
+    }
+}
